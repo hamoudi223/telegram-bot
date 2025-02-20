6b02e4a06527025c0513d4538d77cb3293bcfc71
Dream <166921709+hamoudi223@users.noreply.github.com>
2025-02-20 00:56:48 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,11 +1,11 @@
-global.prefix = '/'; // Set Your Preferred Prefix
+global.prefix = '.'; // Set Your Preferred Prefix
 global.botName = 'GIFTED-MD'; // Bot Name
 global.botVersion = '2.0.0';
-global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
-global.botToken = 'xxxxxxxx:xxxx'; // Replace With Your Bot Token (Get it from BotFather)
+global.botPic = 'https://iili.io/22kpOMv.jpg'; // Can use path, can also use link
+global.botToken = '8148116748:AAGVKbk6km8ttiDp3kLx66_091k0oBdb8bk'; // Replace With Your Bot Token (Get it from BotFather)
 global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For Bot Owner)
-global.timeZone = 'Africa/Nairobi'; // Your Time Zone
-global.ownerId = [xxxxxx]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.timeZone = 'Africa/Mali'; // Your Time Zone
+global.ownerId = [8148116748]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
 global.footer = '*Powered by GiftedTech*';
 global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
